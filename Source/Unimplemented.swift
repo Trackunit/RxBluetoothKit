9babbdaf73d0e3ec65d6c181ed3317c0d76a8b4a
jesperdodensig <5467691+jesperdodensig@users.noreply.github.com>
2023-09-19 15:21:04 +0200

    rxSwift 6.6.0

--- Source/Unimplemented.swift
+++ Source/Unimplemented.swift
@@ -5,7 +5,10 @@ func unimplementedFunction(file: String = #file, function: String = #function, l
     fatalError("Unimplemented function \(function) in \(file):\(line)")
 }
 
+
 extension Observable {
+    public typealias Observable = RxSwift.Observable
+
     static func unimplemented(file: String = #file, function: String = #function, line: Int = #line)
         -> Observable<Element> {
         unimplementedFunction(file: file, function: function, line: line)
